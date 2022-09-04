b782a97f7c696abb62b5e1e20274a9d93f0b85f3
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-04 15:12:45 +0200

    feat(react-router): implemented base router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
 import useFetch from "react-fetch-hook";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import { Hero } from './components/Hero'
+import { Home } from './pages/Home'
+import { GameDetails } from './pages/GameDetails'
+import { Navbar, pageURLS } from './shared/Navbar';
+
 import './App.css';
 
 export default function App() {
@@ -14,21 +18,27 @@ export default function App() {
   );
 
   return (
-    <div className="App bg-slate-900 text-slate-100">
-      <Hero />
-      {/* <h1>API Posts</h1>
-      {isLoading && <div>Please wait =)...</div>}
-      {error && (
-        <div>{`Opps! something went wrong, error: - ${error}`}</div>
-      )}
-      <ul>
-        {gameData &&
-          gameData.map(({ id, title }) => (
-            <li key={id}>
-              <h3>{title}</h3>
-            </li>
-          ))}
-      </ul> */}
-    </div>
+    <Router>
+      <div className="App bg-slate-900 text-slate-100">
+        <Navbar />
+        <Routes>
+          <Route exact path={pageURLS.home} element={<Home />}></Route>
+          <Route exact path={pageURLS.detailsFFVII} element={<GameDetails title={'FFVII'}/>}></Route>
+        </Routes>
+        {/* <h1>Game Data</h1>
+        {isLoading && <div>Please wait =)...</div>}
+        {error && (
+          <div>{`Opps! something went wrong, error: - ${error}`}</div>
+        )}
+        <ul>
+          {gameData &&
+            gameData.map(({ id, title }) => (
+              <li key={id}>
+                <h3>{title}</h3>
+              </li>
+            ))}
+        </ul> */}
+      </div>
+    </Router>
   );
 }
