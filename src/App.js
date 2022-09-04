079b352c00df06571a0a83d6da8641963ef141fa
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-04 14:26:25 +0200

    feat(app-js-start): starting code for appjs, tested useFetch (beautiful btw)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import useFetch from "react-fetch-hook";
+
+import { Hero } from './components/Hero'
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const [gameDetailsFF7, setgameDetailsFF7] = useState(null)
+  const [charactersFF7, setcharactersFF7] = useState(null)
+  
+  const { isLoading, gameData, error } = useFetch(
+    "https://www.moogleapi.com/api/v1/games"
+  );
+
   return (
-    <div className="App">
-      <h2>test</h2>
+    <div className="App bg-slate-900 text-slate-100">
+      <Hero />
+      {/* <h1>API Posts</h1>
+      {isLoading && <div>Please wait =)...</div>}
+      {error && (
+        <div>{`Opps! something went wrong, error: - ${error}`}</div>
+      )}
+      <ul>
+        {gameData &&
+          gameData.map(({ id, title }) => (
+            <li key={id}>
+              <h3>{title}</h3>
+            </li>
+          ))}
+      </ul> */}
     </div>
   );
 }
-
-export default App;
