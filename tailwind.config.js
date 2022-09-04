bc2c68da132e546776a96003100665f85827b1a6
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-04 14:24:10 +0200

    feat(tailwind-config-fonts): added boilerplate for extended font classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+			fontFamily: {
+				'orbi': ['Orbitron', 'Helvetica', 'Arial', 'sans-serif'],
+				'cond': ['Roboto Condensed', 'Helvetica', 'Arial', 'sans-serif'],
+				'main': ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+			},
+		},
   },
   plugins: [],
 }
