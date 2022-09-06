eaf0a49cf1d5371f6b49cb8bf6175c86db4357cc
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-06 23:29:48 +0200

    feat(navbar-refactor): using map to create link items

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,18 +1,20 @@
+import { useContext } from 'react';
+import { GameContext } from '../context/GameContext';
 import { Link } from 'react-router-dom';
 
-export const pageURLS = {
-  home: '/',
-  detailsFFVIII: '/ff-iii-details',
-  detailsFFVII: '/ff-vii-details',
-  detailsFFXV: '/ff-xv-details'
-}
+export const Navbar = ()=> {
+  const { detailPages } = useContext(GameContext);
 
-export const Navbar = ()=> (
-  <nav className="sticky top-0 left-0 w-full flex justify-between z-10">
-    <Link to={pageURLS.home}>Logo</Link>
-    <ul className="flex">
-      <li className="mx-4"><Link to={pageURLS.home}>Home</Link></li>
-      <li className="mx-4"><Link to={pageURLS.detailsFFVII}>FFVII Details</Link></li>
-    </ul>
-  </nav>
-);
+  return (
+    <nav className="sticky top-0 left-0 w-full flex justify-between z-10">
+      <Link to="/">Logo</Link>
+      <ul className="flex">
+        {
+          detailPages.map(({url, navTitle})=>{
+            return <li className="mx-4" key={`${url}${navTitle}`}><Link to={url}>{navTitle}</Link></li>
+          })
+        }
+      </ul>
+    </nav>
+  )
+};
