f1dedf34304644a7ee115df66fa243e33bdaca59
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-06 23:29:14 +0200

    feat(home): passing ids and page details object, added props from game row comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,23 +5,24 @@ import fetchApi from "../hooks/fetchApi";
 
 import { LoadingErrorSuccess } from "../components/LoadingErrorSuccess";
 import { Hero } from "../components/Hero";
-import { GameRow } from "../components/GameRow";
+import { GameRow } from "../components/gameRow/GameRow";
 
 const getGames = () => axios.get('https://www.moogleapi.com/api/v1/games');
 const getCharacters = ()=> axios.get('https://www.moogleapi.com/api/v1/characters');
-// const gamesInPage = ['Final Fantasy 03', 'Final Fantasy 07', 'Final Fantasy 15'];
 
 export const Home = () => {
   const getGamesApi = fetchApi(getGames);
   const { gameSelection, setGameSelection} = useContext(GameContext);
-  
+  const { ffGameIds } = useContext(GameContext);
+  const { detailPages } = useContext(GameContext);
+
   useEffect(() => {
     getGamesApi.request();
   }, []);
   
   useEffect(() => {
-    setGameSelection(getGamesApi.filterGames(getGamesApi.data));
-  }, [getGamesApi.data]);  
+    setGameSelection(getGamesApi.filterGames(getGamesApi.data, ffGameIds.ff3, ffGameIds.ff7, ffGameIds.ff15));
+  }, [getGamesApi.data]);
 
   return (
     <>
@@ -32,7 +33,7 @@ export const Home = () => {
         data={getGamesApi.data} 
         dataSelection={gameSelection}
       >
-        <GameRow data={gameSelection}/>
+        <GameRow selectedGames={gameSelection} selectionGameIds={ffGameIds} isDetailPage={false}/>
       </LoadingErrorSuccess>
     </>
   )
