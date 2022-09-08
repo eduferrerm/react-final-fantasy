0d99e88962a29998b86f3e8b600855a35d66f016
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-08 23:13:44 +0200

    feat(app-js): imported api custom hook, changed list routes in map

--- src/App.js
+++ src/App.js
@@ -1,65 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GameContext } from './context/GameContext'
+import fetchGames from './hooks/fetchGames';
+import './App.css';
+
 import { Home } from './pages/Home'
 import { GameDetails } from './pages/GameDetails'
 import { Navbar } from './shared/Navbar';
-import { GameContext } from './context/GameContext'
-
-import './App.css';
 
 export default function App() {
+  const getGamesApi = fetchGames();
 
-  const [gameSelection, setGameSelection] = useState([]);
-  
-  const ffGameIds = {
-    ff3: '53da5efb-5925-409b-399b-08d6b0a627a3',
-    ff7: '9fc1c377-95f1-4a09-399f-08d6b0a627a3',
-    ff15: '0da69a55-9ab2-4798-39a7-08d6b0a627a3'
-  };
-
-  const detailPages = [
-    { 
-      gameTitle: 'FFIII',
-      url: '/ff-iii-details',
-      pageTitle: 'FFIII | Details',
-      navTitle: 'FFIII',
-      ffGameIdsKey: 'ff3'
-    },
-    { 
-      gameTitle: 'FFVII',
-      url: '/ff-vii-details',
-      pageTitle: 'FFVII | Details',
-      navTitle: 'FFVII',
-      ffGameIdsKey: 'ff7'
-    },
-    { 
-      gameTitle: 'FFVXV',
-      url: '/ff-xv-details',
-      pageTitle: 'FFXV | Details',
-      navTitle: 'FFXV',
-      ffGameIdsKey: 'ff15'
-    },
-  ]
+  useEffect(()=>{
+    getGamesApi.request();
+  },[])
 
   return (
     <GameContext.Provider value={{
-      detailPages,
-      ffGameIds,
-      gameSelection, 
-      setGameSelection
+      getGamesApi
     }}>
       <Router>
         <div className="App bg-slate-900 text-slate-100">
           <Navbar />
           <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route exact path={detailPages[0].url} element={<GameDetails title={detailPages[0].pageTitle} selectedGame={detailPages[0].ffGameIdsKey}/>}
-            >
-            </Route>
-            <Route exact path={detailPages[1].url} element={<GameDetails title={detailPages[1].pageTitle} selectedGame={detailPages[1].ffGameIdsKey}/>}>
-            </Route>
-            <Route exact path={detailPages[2].url} element={<GameDetails title={detailPages[2].pageTitle} selectedGame={detailPages[2].ffGameIdsKey}/>}>
-            </Route>
+            <Route exact path="/"element={<Home />}></Route>
+            {getGamesApi.data && 
+              getGamesApi.data.map((game, idx)=> (
+                <Route 
+                  exact 
+                  path={getGamesApi.gameDetailPages[idx].url} 
+                  element={<GameDetails
+                  title={getGamesApi.gameDetailPages[idx].pageTitle} 
+                  selectedGameID={getGamesApi.gameDetailPages[idx].gameId}/>}>
+                </Route>
+              ))
+            }
           </Routes>
         </div>
       </Router>
