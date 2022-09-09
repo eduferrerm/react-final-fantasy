749ce5f8f5cba62ecce91f6f2d4d7f2b3371bc89
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-09 22:17:39 +0200

    feat(fetch-games-cleanup): moved game detail pages to separate file

--- src/hooks/fetchGames.jsx
+++ src/hooks/fetchGames.jsx
@@ -1,41 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
+import { API_BASE_URL, GAME_DETAIL_PAGES as gameDetailPages } from "../constants";
 
-const getGames = () => axios.get('https://www.moogleapi.com/api/v1/games');
+const fetchGames = () => axios.get(`${API_BASE_URL}/games`);
 
 export default () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-  
-  const gameDetailPages = [
-    { 
-      gameTitle: 'FFIII',
-      url: '/ff-iii-details',
-      pageTitle: 'FFIII | Details',
-      navTitle: 'FFIII',
-      gameId: '53da5efb-5925-409b-399b-08d6b0a627a3'
-    },
-    { 
-      gameTitle: 'FFVII',
-      url: '/ff-vii-details',
-      pageTitle: 'FFVII | Details',
-      navTitle: 'FFVII',
-      gameId: '9fc1c377-95f1-4a09-399f-08d6b0a627a3'
-    },
-    { 
-      gameTitle: 'FFVXV',
-      url: '/ff-xv-details',
-      pageTitle: 'FFXV | Details',
-      navTitle: 'FFXV',
-      gameId: '0da69a55-9ab2-4798-39a7-08d6b0a627a3'
-    },
-  ]
 
   const request = async () => {
     setLoading(true);
     try {
-      const result = await getGames();
+      const result = await fetchGames();
       setData(filterGames(result.data, gameDetailPages[0].gameId, gameDetailPages[1].gameId, gameDetailPages[2].gameId));
     } catch (err) {
       setError(err.message || "Unexpected Error!");
@@ -47,7 +24,9 @@ export default () => {
   const filterGames = (data, ff3ID, ff7ID, ff15ID) => {
     if (data !== null) {
       const filteredGames = data.filter(game => 
-        game.gameId === ff3ID || game.gameId === ff7ID || game.gameId === ff15ID
+        game.gameId === ff3ID || 
+        game.gameId === ff7ID || 
+        game.gameId === ff15ID
       );
       return filteredGames;
     }  
