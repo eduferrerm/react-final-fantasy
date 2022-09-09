047e7b243ebc2a77ddcbe224340eaf59a0ccd83f
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-10 01:28:25 +0200

    feat(tailwind-config): extending current utilities, did it wrong previously

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,18 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    maxWidth: {
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-    },
+  theme: {    
     extend: {
 			fontFamily: {
 				'orbi': ['Orbitron', 'Helvetica', 'Arial', 'sans-serif'],
 				'cond': ['Roboto Condensed', 'Helvetica', 'Arial', 'sans-serif'],
 				'main': ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
 			},
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+      },
 		},
   },
   plugins: [],
