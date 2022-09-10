dfd9c8de2f296eeac6294f935c8d95bae15a9928
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-10 22:11:33 +0200

    feat(button-component): brought link component into new button compenent for reuse

--- src/components/gameRow/GameRow.jsx
+++ src/components/gameRow/GameRow.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { GameContext } from "../../context/GameContext";
-import { Link } from 'react-router-dom';
+import { Button } from "../ui/Button";
 import ff3Image from '../../assets/img/ff-3/ff-3-game-img-1.jpeg';
 import ff7Image from '../../assets/img/ff-7/ff-7-game-img-1.jpeg';
 import ff15Image from '../../assets/img/ff-15/ff-15-game-img-1.jpeg';
@@ -40,28 +40,30 @@ export const GameRow = ({selectedGames, isDetailPage}) => {
   };
 
   return (
-    <ul className="game-row flex flex-col md:flex-row p-4">
-      {
-        selectedGames.map(({ gameId, picture, title, releaseDate, platform, description }) => (
-          <li className={`w-full ${!isDetailPage ? 'md:w-1/3' : 'md:flex'} py-8 md:px-4`} key={`${gameId}`}>
-            <div className={`w-full ${!isDetailPage ? '' : 'md:w-1/2'}`}>
-              <h2 className="mb-2 text-3xl">{title.replace(/0/g, '')}</h2>
-              <h3 className="mb-4">
-                <span>{releaseDate}</span>
-                <span className='text-teal-500'>|</span>
-                <span>{platform}</span>
-              </h3>
-              <img className={`w-full mb-4`} src={returnNewImage(gameId, picture)} alt=""/>
-            </div>
-            <div className={`w-full ${!isDetailPage ? '' : 'md:w-1/2'}`}>
-              <p className={`${!isDetailPage ? 'h-32' : ''} overflow-hidden mb-4`}>{description}</p>
-              {!isDetailPage &&
-                <Link to={returnURL(gameId)}>Learn More</Link>
-              }
-            </div>
-          </li>
-        ))
-      }
-    </ul>
+    <section>
+      <ul className="game-row flex flex-col md:flex-row p-4">
+        {
+          selectedGames.map(({ gameId, picture, title, releaseDate, platform, description }) => (
+            <li className={`w-full ${!isDetailPage ? 'md:w-1/3' : 'md:flex'} py-8 md:px-4`} key={`${gameId}`}>
+              <div className={`w-full ${!isDetailPage ? '' : 'md:w-1/2'}`}>
+                <h2 className="mb-2 text-3xl text-slate-400">{title.replace(/0/g, '')}</h2>
+                <h3 className="mb-4 text-xs">
+                  <span>{releaseDate}</span>
+                  <span className='text-teal-500 mx-2'>|</span>
+                  <span>{platform}</span>
+                </h3>
+                <img className={`w-full mb-4`} src={returnNewImage(gameId, picture)} alt=""/>
+              </div>
+              <div className={`w-full ${!isDetailPage ? '' : 'md:w-1/2'}`}>
+                <p className={`${!isDetailPage ? 'h-32' : ''} overflow-hidden mb-4`}>{description}</p>
+                {!isDetailPage &&
+                  <Button url={returnURL(gameId)} text={"learn more"}></Button>
+                }
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    </section>
   )
 };
