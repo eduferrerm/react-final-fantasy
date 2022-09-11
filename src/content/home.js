b0320f061e18e510782d9379ca948f0c750d0dda
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-11 16:21:32 +0200

    feat(conten-objects): created objects to feed content to each page block

---
+++ src/content/home.js
@@ -0,0 +1,43 @@
+export const CONTENT = {
+  HERO: {
+    title: 'My favourite Final Fantasy Games',    
+  },
+  COMPOSERS: {
+    title: 'A great part of why i love these games',
+    subtitle: 'Music: The soul of video games',
+    items: [
+      {
+        name: 'Nobuo Uematsu',
+        japaneseName: '植松 伸夫',
+        born: [
+          'March 21, 1959', '63', 'Kōchi Prefecture, Japan'
+        ],
+        genres: [
+          'VGM', 'Symphonic Rock', 'Progressive Rock'
+        ],
+        occupation: [
+          'Composer', 'Keyboardist'
+        ],
+        wikiLink:'https://en.wikipedia.org/wiki/Nobuo_Uematsu',
+        spotifyLink:'https://open.spotify.com/artist/3V79CTgRnsDdJSTqKitROv',
+        spotifyEmbedID:'2x9uZkJTGLxXHDm8rztQGK',
+      },
+      {
+        name: 'Yoko Shimomura',
+        japaneseName: '下村 陽子',
+        born: [
+          'October 19, 1967', '54', 'Hyōgo Prefecture, Japan'
+        ],
+        genres: [
+          'VGM', 'Electronic', 'Symphonic'
+        ],
+        occupation: [
+          'Composer', 'Pianist'
+        ],
+        wikiLink:'https://en.wikipedia.org/wiki/Yoko_Shimomura',
+        spotifyLink:'https://open.spotify.com/artist/2uDsUIyCIqk9wKj17I8WAH',
+        spotifyEmbedID:'2zKqD5DUNUE4isjGLixU8v',
+      },
+    ],
+  } 
+};
