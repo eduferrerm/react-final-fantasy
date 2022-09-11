875716ad04f690547d84a0df3b3603fa58e8634a
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-09-11 20:16:10 +0200

    feat(character-row): component tweaks and style changes

--- src/components/characterRow/CharacterRowImg.jsx
+++ src/components/characterRow/CharacterRowImg.jsx
@@ -2,13 +2,15 @@ import unkownCharacterImage from '../../assets/img/unkown-character.jpeg';
 import './characterRow.scss'
 
 export const CharacterRowImg = ({src, name}) => (
-  <img 
-    className='character-row__img my-4'
-    src={src !== undefined 
-      ? src.url 
-      : unkownCharacterImage
-    } 
-    alt={`An image of ${name}`} />
+  <div className='bg-white my-4'>
+    <img 
+      className='character-row__img'
+      src={src !== undefined 
+        ? src.url 
+        : unkownCharacterImage
+      } 
+      alt={`An image of ${name}`} />
+  </div>
 )
   
   
